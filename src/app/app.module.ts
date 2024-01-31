ca00081db39c9f54adb5e7ed27c7febf00f3dd06
Javier <javierfgg.96@gmail.com>
2024-01-30 22:04:31 -0300

    act-e-30

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { InicioComponent } from './templates/inicio/inicio.component';
+import { ProductosComponent } from './templates/productos/productos.component';
+import { PrimeroComponent } from './1-inicio-componentes/primero/primero.component';
+import { SegundoComponent } from './1-inicio-componentes/segundo/segundo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    InicioComponent,
+    ProductosComponent,
+    PrimeroComponent,
+    SegundoComponent
   ],
   imports: [
     BrowserModule,
